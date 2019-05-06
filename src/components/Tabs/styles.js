29557213c9df1983b571ac6e28c5b7a989c44f11
Luciano <dev03@grupog2i.com.br>
2019-05-06 11:39:21 -0400

    App finalizado

---
+++ src/components/Tabs/styles.js
@@ -0,0 +1,28 @@
+import { Animated } from 'react-native';
+import styled from 'styled-components';
+
+export const Container = styled(Animated.View)`
+  height: 100px;
+  margin-top: 20px;
+`;
+
+export const TabsContainer = styled.ScrollView.attrs({
+  contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const TabsItem = styled.View`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  margin-left: 10px;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+export const TabText = styled.Text`
+  font-size: 13px;
+  color: #FFF;
+`;
